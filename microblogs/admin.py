4239dfe3048dc8a0727d7d830cc399d5e35b2ac1
Taseen Rahman <taseen.rahman@kcl.ac.uk>
2021-10-21 12:42:35 +0100

    define the requirements list

--- microblogs/admin.py
+++ microblogs/admin.py
@@ -1,3 +1,11 @@
+#Configuration of the adminstrative user interface
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'username','first_name','last_name','email','is_active'
+    ]
